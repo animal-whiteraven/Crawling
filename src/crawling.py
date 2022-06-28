def6450d5fa682c7a19ea46ada546fc97b71d786
yssong <yssong@osteosys.com>
2022-06-28 15:52:21 +0900

    FEAT: Get target element data from URL page
    Get coin price data

--- src/crawling.py
+++ src/crawling.py
@@ -9,5 +9,10 @@ if __name__ == '__main__':
     driver.get('https://www.binance.com/ko/futures/XRPUSDT') # open url
     driver.implicitly_wait(5) # wait page loading
 
+    for i in range(0, 5):
+        price = driver.find_element('xpath' ,'//*[@id="futuresOrderbook"]/div[3]/div[2]/div[1]') # get xpath element
+        print(price.text) # print price
+        time.sleep(5) #wait 5 sec
+
     time.sleep(10) # wait 10 sec
     driver.close()
