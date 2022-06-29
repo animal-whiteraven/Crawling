d8c1bacefba515622ad07b61dc0ef78607c2cf90
yssong <yssong@osteosys.com>
2022-06-29 18:18:45 +0900

    FEAT: Use mongodb
    Add mongodb test function

--- readme.txt
+++ readme.txt
@@ -5,3 +5,9 @@
 # installed library
   * selenium
     . pip install selenium
+  * mongodb
+    . pip install pymongo
+
+# docker
+  * docker pull mongo
+  * docker run --name crawling-container -v D:\Dev\toy_project\Crawling\db:/data/db -d -p 27017:27017 mongo
--- src/crawling.py
+++ src/crawling.py
@@ -1,8 +1,10 @@
 from selenium import webdriver
 import os
 import time
+from pymongo import MongoClient
+from datetime import datetime
 
-if __name__ == '__main__':
+def selenium_test():
     chromedriver = os.path.dirname(os.path.abspath(__file__)) + "/../chromedriver.exe" # download chromedriver path
     driver = webdriver.Chrome(chromedriver)
 
@@ -16,3 +18,32 @@ if __name__ == '__main__':
 
     time.sleep(10) # wait 10 sec
     driver.close()
+
+
+def mongodb_test():
+    client = MongoClient(host='localhost', port=27017)
+    db = client['coin']
+
+    db.coin.delete_many({})
+
+    for i in range(0, 5):
+        data = {
+            'coin_name' : 'XRP',
+            'date' : datetime.now(),
+            'price' : 0
+        }
+
+        db.coin.insert_one(data)
+
+    print("count : " + str(db['coin'].count_documents({})))
+    for d in db['coin'].find():
+        print(d)
+
+    db.coin.delete_many({})
+
+    print("count : " + str(db['coin'].count_documents({})))
+
+
+if __name__ == '__main__':
+    # selenium_test()
+    mongodb_test()
